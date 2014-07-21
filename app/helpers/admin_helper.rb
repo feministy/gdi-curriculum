a0878759fce4c4a6fe611a3df51cedcab19ea633
Liz Abinante <eabinante@gmail.com>
2014-07-21 13:59:13 -0500

    can_review?, reviewed_by?

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,4 +2,12 @@ module AdminHelper
   def can_update_status?(material)
     is_admin? && !material.new_record?
   end
+
+  def can_review?(material)
+    is_admin? && !reviewed_by?(current_user, material)
+  end
+
+  def reviewed_by?(user, material)
+    user.reviews.any? && user.reviews.pluck(:materials_id).include?(material)
+  end
 end
