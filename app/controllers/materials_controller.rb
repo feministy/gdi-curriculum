8d87452e1711b0c488dfadbe9f58aa256b3b62b2
Liz Abinante <eabinante@gmail.com>
2014-07-21 15:35:42 -0500

    core view, organizing reviews form

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -63,6 +63,10 @@ class MaterialsController < ApplicationController
     end
   end
 
+  def core
+    @materials = Material.where(status: 'Core')
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_material
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get '/materials/core', to: 'materials#core', as: 'core_materials_path'
+  root 'materials#core'
+
   resources :materials do
     resources :reviews
   end
 
-  # WIP
-  # get '/materials/core', to: 'materials#core', as: 'core_materials_path'
 
   resources :users
   get '/users/:id/reviews', to: 'users#reviews', as: 'user_reviews'
