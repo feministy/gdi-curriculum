721307ab2aaff0966d37645fe4146b84dc025679
Liz Abinante <eabinante@gmail.com>
2014-07-21 11:13:35 -0500

    logging in works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      create_session
+      redirect_to @user
+    elsif @user.nil?
+      @error = "An account with the email address #{params[:email]} not found."
+    else
+      @error = "Authentication failed."
+    end
+  end
+
+  def destroy
+    destroy_session
+    redirect_to new_session_path
+  end
 end
