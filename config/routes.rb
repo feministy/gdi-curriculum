1f7d4cf3262ffa69f052d3b54ef78c112e8957b7
Liz Abinante <eabinante@gmail.com>
2014-07-21 12:28:05 -0500

    my reviews route for admins

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :materials
 
   resources :users
+  get '/users/:id/reviews', to: 'users#reviews', as: 'user_reviews'
 
   resource :sessions, only: [:new, :create, :destroy]
 end
