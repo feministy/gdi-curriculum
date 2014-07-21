9a71370e407126392075de0ec05ea68bd054b8b4
Liz Abinante <eabinante@gmail.com>
2014-07-21 14:47:10 -0500

    wip for core route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  # WIP
+  # get '/materials/core', to: 'materials#core', as: 'core_materials_path'
+
   resources :users
   get '/users/:id/reviews', to: 'users#reviews', as: 'user_reviews'
 
